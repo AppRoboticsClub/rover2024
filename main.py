8c1e5c15ab6f429854e7cb10950592241043ef69
Hermes <hermeshenry@ieee.org>
2026-03-05 22:19:18 +0000

    Started 2026 Refactor
    Reintroduced main.py, templates/index.html, and relevant assets but stripped back with more comments and some features removed, either permanently or with the intent of adding back later. Will debug and strip back motors.py directly on the pi as that file relies on GPIO.

---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+# Following two imports are just for more
+#   informative debug prints with timestamps
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+# import motors
+
+# Declare a global motion dictionary to keep track
+#   of the current speed and turning value of the rover
+#   at any given moment.
+motion: dict[str, float] = {
+    'speed': 0,
+    'turn': 0
+}
+
+
+"""
+Create the Flask webapp and use the SocketIO
+  extension to handle use pressing keys.
+"""
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+"""
+Displays the main page
+  - Flask will only search in the `templates` directory
+      for anything passed to `render_template(...)`
+"""
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+"""
+Handle any key being pressed
+"""
+@socketio.on('key-down')
+def handle_key(data, m = 1):
+    prev = motion.copy()
+    match data:
+        case 'w': motion['speed'] = -0.75 * m
+        case 'a': motion['turn']  = -1 * m
+        case 's': motion['speed'] = 0.75 * m
+        case 'd': motion['turn']  = 1 * m
+        case _: print("UNDEFINED KEY PRESS")
+
+    # Check if the motion was changed so we don't print
+    #   any extra debug info while a key is being pressed.
+    if motion != prev:
+        timestamp = datetime.now(ZoneInfo("America/New_York")).strftime("%H:%M:%S.%f")
+        action = "press" if m else "release"
+        print(f"{timestamp} {f'[{action}]':9s} `{data}` | speed ={motion['speed']:>5.2f}  turn ={motion['turn']:>5.2f}    ")#, end='\r', flush=True)
+
+    # motors.acc(motion['speed'])
+    # motors.turn(motion['turn'])
+
+"""
+Handle any key being released
+"""
+@socketio.on('key-up')
+def handle_key_up(data):
+    handle_key(data, 0)
+
+"""
+Asks SocketIO to run the Flask webapp when this
+  file (main.py) is run by python: `python main.py`
+"""
+if __name__ == '__main__':
+    """
+    `host="0.0.0.0"` Makes the Flask webapp available to the entire
+      network, aka the hotspot that this pi is running.
+    `debug=False` Disables the Flask debugger, which is a critical
+       security vulnerability and allows for arbitrary code execution.
+       This is still useful for development, but shouldn't be sent to the rover enabled.
+    `port=5001` Tells Flask to host the webapp on port 5001 instead of the default
+       of 5000 since that port is reserved by Apple devices for AirPlay.
+    """
+    socketio.run(app, debug=False, host="0.0.0.0", port=5001)
+    # TODO: Make sure to turn off the debug flag.
---
+++ motors.py
@@ -0,0 +1,90 @@
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)  # use broadcom pin numbering (not board pin numbering)
+
+# Define pins
+pinMotorL = 13 # GPIO Pin for motorL # previously motor1
+pinMotorR = 12 # GPIO Pin for motorR # previously motor2
+pinReverseL = 5 # pin for reverse left
+pinReverseR = 6 # pin for reverse right
+
+# setup
+# motor control pins
+GPIO.setup(pinMotorL, GPIO.OUT) # set pin motorL to output
+GPIO.setup(pinMotorR, GPIO.OUT) # set pin motorR to output
+
+# reverse control pins
+GPIO.setup(pinReverseL, GPIO.OUT) # set pin reverseL to output
+GPIO.setup(pinReverseR, GPIO.OUT) # set pin reverseR to output
+GPIO.output(pinReverseL, False) # set so no pin output, aka go forwards
+GPIO.output(pinReverseR, False) # set so no pin output, aka go forwards
+
+motorLServo = GPIO.PWM(pinMotorL, 1000) # set Motors to PWM. Change this depeneding on how your motor controller works
+motorLServo.start(8)
+motorRServo = GPIO.PWM(pinMotorR, 1000) # set Motors to PWM. Change this depeneding on how your motor controller works
+motorRServo.start(8)
+motorLServo.ChangeDutyCycle(0) # 
+motorRServo.ChangeDutyCycle(0) # RPI can encounter a bug that might require this to be applied twice
+dutyL = 5
+dutyR = 5
+
+isReversed = False
+
+def turn(value): 
+    motorLServo.ChangeDutyCycle(speed * (1 + value)) # 50 Right, 0 Neutral, -50 Left
+    motorRServo.ChangeDutyCycle(speed * (1 - value))
+    dutyL = 50 + value
+    dutyR = 50 - value
+    
+def acc(value):
+    global isReversed
+    if value < 0:
+        setReverse(True)
+    else:
+        setReverse(False)
+    # check for is reversed
+    
+    # reduce reverse speed
+    if isReversed:
+        value = value * -1
+        value = value / 5
+
+    # update pwm duty cycle to new value
+    motorLServo.ChangeDutyCycle(value)
+    motorRServo.ChangeDutyCycle(value)
+    dutyL = value
+    dutyR = value
+
+    # update the speed
+    global speed
+    speed = value
+
+def stop():
+    motorLServo.stop()
+    motorRServo.stop()
+    
+def start():
+    motorLServo.start(dutyL)
+    motorRServo.start(dutyR)
+    
+def Left(value: int):
+    # motorLServo.ChangeFrequence(value)
+    motorLServo.ChangeFrequency(value)
+    
+def Right(value: int):
+    # motorRServo.ChangeFrequence(value)
+    motorRServo.ChangeFrequency(value)
+
+def setReverse(isRev: bool):
+    '''
+    - Takes a boolean value as a parameter as to whether the rover should be reversed, then updates
+    the reverse pins to be on if reverse is true, and off if false
+    - @param isRev: bool - The current state of the reverse button
+    '''
+    global isReversed
+    if isReversed == isRev:
+        return
+    isReversed = isRev
+    GPIO.output(pinReverseL, isRev)
+    GPIO.output(pinReverseR, isRev)
